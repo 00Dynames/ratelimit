efa29f88c94c7deb24a0d248fd7a824f091b3b45
Dennis <13833850+00Dynames@users.noreply.github.com>
2020-01-29 16:53:43 +1100

    added basic rate limiter

--- ratelimit.go
+++ ratelimit.go
@@ -1,17 +1,35 @@
 package ratelimit
 
 import (
+	"fmt"
+	"golang.org/x/time/rate"
 	"net/http"
 )
 
 type Limiter struct {
-	limit int
+	l *rate.Limiter
 }
 
 func NewLimiter(limit int) *Limiter {
-	return &Limiter{limit}
+	fmt.Println(limit)
+	return &Limiter{rate.NewLimiter(rate.Limit(limit), 4)}
 }
 
-func (*Limiter) Limit(handler http.Handler) http.Handler {
-	return nil
+func (limiter *Limiter) Limit(handler http.Handler) http.Handler {
+	limiter.l.Limit()
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			delay := limiter.l.Reserve().Delay()
+			if delay != 0 {
+				http.Error(
+					w,
+					fmt.Sprintf("Rate limit exceeded. Try again in %v", delay),
+					http.StatusTooManyRequests,
+				)
+				return
+			}
+
+			handler.ServeHTTP(w, r)
+		},
+	)
 }
