b589ddee202022389d22d5883883743ac8327793
Dennis <13833850+00Dynames@users.noreply.github.com>
2020-01-29 20:41:46 +1100

    added doc comments for ratelimite package

--- ratelimit.go
+++ ratelimit.go
@@ -1,3 +1,4 @@
+// Package ratelimit provides a rate limiter for use with http requests
 package ratelimit
 
 import (
@@ -6,14 +7,21 @@ import (
 	"net/http"
 )
 
+// A Limiter wraps a Limiter struct from the rate package
+// and provides a method Limit that checks if the rate limit
+// has been exceeded for the current request.
 type Limiter struct {
 	l *rate.Limiter
 }
 
+// NewLimiter takes a limit that represents requests per second (rps)
+// and returns an instance of a Limiter struct.
 func NewLimiter(limit int) *Limiter {
 	return &Limiter{rate.NewLimiter(rate.Limit(limit), 4)}
 }
 
+// Limit takes anything that implements the http.Handler interface and returns
+// a handler function that serves the request.
 func (limiter *Limiter) Limit(handler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
